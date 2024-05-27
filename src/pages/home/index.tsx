e34164739c47637c94fd4e3183a4993d77d8ecd4
amin75t <167482540+amin75t-work@users.noreply.github.com>
2024-05-27 16:03:49 +0330

    Update: pagination finished

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
+import {Pagination} from "@/components/pagination/pagination.tsx";
 
 const Home = () => {
     return (
         <>
 
-
+<Pagination/>
             <h1>home</h1>
         </>
     )
